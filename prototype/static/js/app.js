39a96d290c516deceaa0d1bb0de966dbbfc8a515
d.bitter <d.bitter@viriciti.nl>
2017-03-07 15:36:24 +0100

    base to work from

--- prototype/static/js/app.js
+++ prototype/static/js/app.js
@@ -7,14 +7,100 @@
         MAPBASEURL: 'http://mt1.funda.nl/maptiledata.ashx',
         APIKEY: '271175433a7c4fe2a45750d385dd9bfd/',
         SEARCHURL: '?type=koop&zo=/amsterdam/tuin/&page=1&pagesize=25',
-        OBJECTURL: 'detail/271175433a7c4fe2a45750d385dd9bfd/koop/643335c8-380f-4350-b0df-dff5b6da1573/',
+        OBJECTURL: 'detail/',
         AUTOSUGGESTURL: '/geo/suggest/?query=amsterdam&max=7&type=koop&callback=callback',
-        MAPURL: '?z=7&x=66&y=42&zo=koop/heel-nederland&callback=callback'
+        MAPURL: '?z=7&x=66&y=42&zo=koop/heel-nederland&callback=callback',
+        GUIDURL: ' http://funda.kyrandia.nl/tinyId/'
     }
-    // get data
-    var url = config.BASEURL + config.APIKEY + config.SEARCHURL;
-    console.log(url)
-    aja().url(url).on('success', function(data) {
-        console.log(data)
-    }).go();
+    var sections = {
+        preferences: document.querySelector('#preferences'),
+        preferencesInputs: document.querySelector('#preferencesInputs'),
+        map: document.querySelector('#map'),
+        detail: document.querySelector('#detail')
+    }
+    var templates = {
+        preferences: document.querySelector('#templatePreferences'),
+        map: document.querySelector('#templateMap'),
+        listItem: document.querySelector('#templateListItem'),
+        detail: document.querySelector('#templateDetail')
+    }
+    var preferences = {
+        coffeeshop: {
+            lable: "Koffieshops",
+            value: 9
+        },
+        bar: {
+            lable: "Bars",
+            value: 8
+        },
+        gym: {
+            lable: "Sportscholen",
+            value: 6
+        },
+        park: {
+            lable: "Parken",
+            value: 7
+        },
+        school: {
+            lable: "Scholen",
+            value: 2
+        }
+    }
+    var renderPreferences = function() {
+        sections.preferences.classList.remove('hide');
+        sections.map.classList.add('hide');
+        var preferencesInputs = sections.preferencesInputs,
+            preferencesTemplate = templates.preferences,
+            source = preferencesTemplate.innerHTML,
+            compile = Handlebars.compile(source),
+            html = '';
+        Object.keys(preferences).map(function(objectKey, index) {
+            var value = preferences[objectKey];
+            html = compile(value);
+            preferencesInputs.innerHTML += html;
+        });
+    }
+    var renderMap = function() {
+        sections.preferences.classList.add('hide');
+        sections.map.classList.remove('hide');
+        var map = sections.map,
+            mapTemplate = templates.map,
+            source = mapTemplate.innerHTML,
+            compile = Handlebars.compile(source),
+            html = '';
+        html = compile();
+        map.innerHTML += html;
+    }
+    var renderDetail = function(id) {
+        sections.preferences.classList.add('hide');
+        sections.map.classList.add('hide');
+        sections.detail.classList.remove('hide');
+        sections.detail.innerHTML = ''
+        var url = config.GUIDURL + id
+        aja().url(url).on('success', function(property) {
+            var guid = property[0].intid
+            var url = config.BASEURL + config.OBJECTURL + config.APIKEY + "koop/" + guid
+            aja().url(url).on('success', function(detailData) {
+                var detail = sections.detail,
+                    detailTemplate = templates.detail,
+                    source = detailTemplate.innerHTML,
+                    compile = Handlebars.compile(source),
+                    html = '';
+                html = compile(detailData);
+                detail.innerHTML += html;
+            }).go();
+        }).go();
+    }
+    // routie
+    routie({
+        '': function(id) {
+            renderPreferences();
+        },
+        'map': function() {
+            renderMap();
+        },
+        'detail/:id': function(id) {
+            renderDetail(id);
+        }
+    });
 })();
---
+++ prototype/static/js/funda-maps.js
@@ -0,0 +1,179 @@
+function FundaTest(oOptions) {
+    this.opt = oOptions;
+    this.TILE_WIDTH = 256;
+    this.TILE_HEIGHT = 256;
+    this.MARKER_WIDTH = 32;
+    this.MARKER_HEIGHT = 32;
+    this.MARKER_ICON = 'http://www.fundainbusiness.nl/img/kaart/marker/marker-small.png';
+    this.init()
+}
+FundaTest.prototype.init = function() {
+    this.oTargetDoc = document.getElementById(this.opt.sTargetContainerId);
+    if (this.oTargetDoc == null) alert('Could not find target container');
+    this.oDataTiles = {};
+}
+FundaTest.prototype.doStuff = function() {
+    // Get a detail object.
+    this.loadDataWrapper(this.opt.sAanbodApiBasePath + '/detail/' + this.opt.sApiKey + '/koop/643335c8-380f-4350-b0df-dff5b6da1573/', 'onReceiveDetailObject');
+    // Get a search query result.
+    this.loadDataWrapper(this.opt.sAanbodApiBasePath + '/' + this.opt.sApiKey + '/?type=koop&zo=/amsterdam/tuin/&page=1&pagesize=25', 'onReceiveSearchResult');
+};
+FundaTest.prototype.onReceiveDetailObject = function(oObjectDetails, sError) {
+    if (sError != null) {
+        alert('Something went wrong: ' + sError);
+        return;
+    }
+    var oNewSection = document.createElement('div');
+    oNewSection.innerHTML = '<h2>Object details example</h2>' + '<b>Address:</b> ' + oObjectDetails.Adres + '<br />' + '<b>ID:</b> ' + oObjectDetails.InternalId;
+    this.oTargetDoc.appendChild(oNewSection);
+};
+FundaTest.prototype.onReceiveSearchResult = function(oSearchResult, sError) {
+    if (sError != null) {
+        alert('Something went wrong: ' + sError);
+        return;
+    }
+    var oNewSection = document.createElement('div');
+    var sResult = '<h2>Object search example</h2><ul>';
+    for (var i = 0, n = oSearchResult.Objects.length; i < n; i++) {
+        var myObject = oSearchResult.Objects[i];
+        sResult += '<li>' + myObject.Adres + '</li>';
+    }
+    sResult += '</ul>';
+    oNewSection.innerHTML = sResult;
+    this.oTargetDoc.appendChild(oNewSection);
+};
+FundaTest.prototype.initMap = function() {
+    var mapDiv = document.getElementById(this.opt.sMapId);
+    this.oMap = new google.maps.Map(mapDiv, {
+        center: {
+            lat: 52.353344,
+            lng: 4.896417
+        },
+        zoom: 14
+    });
+
+
+   sSearchQuery = 'koop/heel-nederland';
+    //this.GenerateMapFromImageTiles(this.oMap, sSearchQuery);
+    this.GenerateMapFromDataTiles(this.oMap, sSearchQuery);
+}
+FundaTest.prototype.GenerateMapFromDataTiles = function(oMap, sSearchQuery) {
+    var iTileWidth = this.TILE_WIDTH;
+    var iTileHeight = this.TILE_HEIGHT;
+    var iMarkerWidth = this.MARKER_WIDTH;
+    var iMarkerHeight = this.MARKER_HEIGHT;
+    var sMarkerIcon = this.MARKER_ICON;
+    // Instantiate our custom map tile handler.
+    var oMapType = new FundaMapType(new google.maps.Size(iTileWidth, iTileHeight), sSearchQuery, function(oTileData, sDivId) {
+        var oMapTile = document.getElementById(sDivId);
+        var propertyList = document.getElementById('property-list');
+        propertyList.innerHTML = '';
+        for (var i = 0, n = oTileData.points.length; i < n; i++) {
+            // This is the object we've found.
+            var oPoint = oTileData.points[i];
+            var propertyListItemTemplate = document.getElementById('templateListItem'),
+                source = propertyListItemTemplate.innerHTML,
+                compile = Handlebars.compile(source),
+                html = '';
+            html = compile(oPoint);
+            propertyList.innerHTML += html;
+            // Create and position an HTML element.
+            var marker = document.createElement('img');
+            marker.style.position = 'absolute';
+            marker.style.left = Math.floor(-(iMarkerWidth / 2) + iTileWidth * ((oPoint.x - oTileData.lng) / oTileData.spanlng)) + 'px';
+            marker.style.top = Math.floor(-(iMarkerHeight / 2) + iTileHeight - iTileHeight * ((oPoint.y - oTileData.lat) / oTileData.spanlat)) + 'px';
+            marker.src = sMarkerIcon;
+            oMapTile.appendChild(marker);
+        }
+    });
+    // Use this map type as overlay.
+    oMap.overlayMapTypes.insertAt(0, oMapType);
+}
+// Create an overlay of image map tiles.
+FundaTest.prototype.GenerateMapFromImageTiles = function(oMap, sSearchQuery) {
+    var oMapType = new google.maps.ImageMapType({
+        getTileUrl: function(coord, zoom) {
+            return 'http://mt1.funda.nl/maptileimage.ashx?z=' + zoom + '&x=' + coord.x + '&y=' + coord.y + '&mode=png&zo=' + sSearchQuery;
+        },
+        tileSize: new google.maps.Size(this.TILE_WIDTH, this.TILE_HEIGHT),
+        isPng: true
+    });
+    oMap.overlayMapTypes.insertAt(0, oMapType);
+};
+/*
+    Helper methods.
+*/
+FundaTest.prototype.loadDataWrapper = function(sUrl, sCallbackMethod) {
+    this.tmpMethod = window[this.opt.sInstanceName].getXmlHttpDocument;
+    this.tmpMethod(sUrl, this[sCallbackMethod]);
+    delete this.tmpMethod;
+};
+FundaTest.prototype.getXmlHttpDocument = function(sUrl, funcCallback) {
+    if (!window.XMLHttpRequest) return null;
+    if (typeof JSON == 'undefined' || !('parse' in JSON)) return null;
+    if (!('call' in funcCallback) || typeof funcCallback.call == 'undefined') return null;
+    var oMyDoc = new XMLHttpRequest();
+    var oCaller = this;
+    oMyDoc.onreadystatechange = function() {
+        if (oMyDoc.readyState != 4) return;
+        var oResponse = null,
+            sError = null;
+        if (oMyDoc.status != 200) sError = 'Connection error: ' + oMyDoc.status + ' ' + oMyDoc.statusText;
+        if (oMyDoc.responseText == null) sError = 'Invalid response';
+        else {
+            try {
+                oResponse = JSON.parse(oMyDoc.responseText);
+            } catch (e) {
+                sError = 'Error parsing JSON: ' + e;
+            }
+        }
+        funcCallback.call(oCaller, oResponse, sError);
+    };
+    oMyDoc.open('GET', sUrl, true);
+    oMyDoc.send(null);
+    return oMyDoc;
+};
+/*
+    FundaMapType
+    Customized map type that can be used as overlay of a regular Google map.
+*/
+function FundaMapType(tileSize, sSearchQuery, funcCallback) {
+    this.tileSize = tileSize;
+    this.funcCallback = funcCallback;
+}
+FundaMapType.prototype.maxZoom = 19;
+FundaMapType.prototype.name = 'Tile #s';
+FundaMapType.prototype.alt = 'Tile Coordinate Map Type';
+FundaMapType.prototype.getTile = function(coord, zoom, ownerDocument) {
+    var div = ownerDocument.createElement('div');
+    div.id = 'funda_tile_' + zoom + '_' + coord.x + '-' + coord.y;
+    div.innerHTML = coord;
+    div.style.width = this.tileSize.width + 'px';
+    div.style.height = this.tileSize.height + 'px';
+    div.style.fontSize = '10';
+    div.style.borderStyle = 'solid';
+    div.style.borderWidth = '1px';
+    div.style.borderColor = '#AAAAAA';
+    div.style.backgroundColor = 'transparent';
+    var funcCallback = this.funcCallback;
+    this.getJsonpDoc('http://mt2.funda.nl/maptiledata.ashx?z=' + zoom + '&x=' + coord.x + '&y=' + coord.y + '&zo=' + sSearchQuery, function(oJson) {
+        funcCallback(oJson, div.id);
+    })
+    return div;
+};
+FundaMapType.prototype.loadJsonpWrapper = function(sUrl, sCallbackMethod) {
+    this.tmpMethod = window[this.opt.sInstanceName].getJsonpDoc;
+    this.tmpMethod(sUrl, this[sCallbackMethod]);
+    delete this.tmpMethod;
+}
+FundaMapType.prototype.getJsonpDoc = function(sUrl, funcCallback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        funcCallback(data);
+    };
+    var script = document.createElement('script');
+    script.src = sUrl + (sUrl.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
